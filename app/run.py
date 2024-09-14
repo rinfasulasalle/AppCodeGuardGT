e914a00b2a6a8b9d9e91f3d1feb1407d1f8e704e
Roger Infa <rinfas@ulasalle.edu.pe>
2024-09-14 05:27:50 -0500

    Creo app y sincronizo bd

---
+++ app/run.py
@@ -0,0 +1,9 @@
+from config import create_app, db
+
+app = create_app()
+
+with app.app_context():
+    db.create_all()  # Crea las tablas si no existen
+
+if __name__ == '__main__':
+    app.run(debug=True)
