2df760e4599f3072dfbd870604ab413bda53ba11
rinfasulasalle <rinfas@ulasalle.edu.pe>
2024-10-09 02:43:08 +0000

    Creacion de index  para cada rol

--- app/routes/r_home.py
+++ app/routes/r_home.py
@@ -1,9 +1,16 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
-
+from flask_login import login_required, current_user
 home = Blueprint('home', __name__)
 
 @home.route('/')
 @login_required
 def get_home():
-    return render_template('home/index.html')
+    # Redirigir a la plantilla correspondiente según el rol del usuario
+    if current_user.role == 'Administracion':
+        return render_template('home/admin/index.html')
+    elif current_user.role == 'Docente':
+        return render_template('home/docente/index.html')
+    elif current_user.role == 'Estudiante':
+        return render_template('home/estudiante/index.html')
+    else:
+        return render_template('home/index.html')
