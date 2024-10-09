d3411038d1c5b08446dd412e1a5ca988c3b12aeb
rinfasulasalle <rinfas@ulasalle.edu.pe>
2024-10-09 14:48:08 +0000

    Vista Conteo  Usuarios

---
+++ app/static/assets/js/gestio_roles.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    function actualizarContadores() {
+      $.get('/usuarios/count', function (data) {
+          $('#sin-rol h3').text(data.cantidad);
+      });
+      $.get('/administracion/count', function (data) {
+          $('#administracion h3').text(data.cantidad);
+      });
+      $.get('/docente/count', function (data) {
+          $('#docente h3').text(data.cantidad);
+      });
+      $.get('/estudiante/count', function (data) {
+          $('#estudiante h3').text(data.cantidad);
+      });
+    }
+
+    // Llama a la función para actualizar los contadores al cargar la página
+    actualizarContadores();
+
+    // También puedes hacer que se actualicen automáticamente cada cierto tiempo
+    setInterval(actualizarContadores, 5000); // Actualiza cada 60 segundos
+  });
