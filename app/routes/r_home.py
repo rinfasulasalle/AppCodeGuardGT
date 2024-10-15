d2f6b42ac8b078ac4037fee11f1bc365e1653cdc
rinfasulasalle <rinfas@ulasalle.edu.pe>
2024-10-15 16:50:23 +0000

    ruta de acceso denegado

--- app/routes/r_home.py
+++ app/routes/r_home.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 home = Blueprint('home', __name__)
 
+def check_access(role):
+    if current_user.role != role:
+        return render_template('home/access_denied.html')
+
 @home.route('/')
 @login_required
 def get_home():
@@ -25,19 +29,21 @@ def get_profile():
 def admin_change_password():
     if current_user.role == 'Administracion':
         return render_template('home/admin/admin_change_password.html')
+    return check_access('Administracion')
 
 @home.route('/gestion_usuarios')
 @login_required
 def get_gestion_usuarios():
     if current_user.role == 'Administracion':
         return render_template('home/admin/gestion_usuarios.html')
-    
+    return check_access('Administracion')
+
 @home.route('/gestion_roles')
 @login_required
 def get_gestion_roles():
     if current_user.role == 'Administracion':
         return render_template('home/admin/gestion_roles.html')
-
+    return check_access('Administracion')
 
 # Para estudiantes
 @home.route('/mis_cursos')
@@ -45,3 +51,4 @@ def get_gestion_roles():
 def mis_cursos():
     if current_user.role == 'Estudiante':
         return render_template('home/estudiante/mis_cursos.html')
+    return check_access('Estudiante')
